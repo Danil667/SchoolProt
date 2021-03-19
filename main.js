8b9f24f30c53f3e57ecbdea960d590a08dcd4bf5
Danil667 <55904678+Danil667@users.noreply.github.com>
2021-03-19 19:37:40 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+"use strict";
 let modal = document.querySelector('.modal'),
 modalOverlay = document.querySelector('.modal-guts')
 let closeButton = document.querySelector('#close-button')
@@ -7,34 +8,41 @@ let authButton = document.querySelector(".Auth");
 let registrModal = document.querySelector("#RegistrModal");
 let authModal = document.querySelector("#AuthModal");
 
-closeButton.addEventListener("click", function(){
- modal.classList.toggle("closed");
- console.log(modal);
- modalOverlay.classList.toggle("closed");
-}); 
-
-registrButton.addEventListener("click", function(){
- modal.classList.toggle("closed");
- console.log(modal);
- modalOverlay.classList.toggle("closed");
-});
-authButton.addEventListener("click", function(){
-    modal.classList.toggle("closed");
-    console.log(modal);
-    modalOverlay.classList.toggle("closed");
-});
-registrModal.addEventListener("click", function(){
-    let pass = prompt("Введите пароль администратора", );
-    if (pass == '123456789'){
-        alert("Регистрация прошла успешно")
-    }
-})
+if(closeButton != null){
+        closeButton.addEventListener("click", function(){
+        modal.classList.toggle("closed");
+        modalOverlay.classList.toggle("closed");
+    }); 
+}
+if(registrButton != null){
+    registrButton.addEventListener("click", function(){
+        modal.classList.toggle("closed");
+        console.log(modal);
+        modalOverlay.classList.toggle("closed");
+    });
+}
+if(authButton != null){
+    authButton.addEventListener("click", function(){
+        modal.classList.toggle("closed");
+        console.log(modal);
+        modalOverlay.classList.toggle("closed");
+    });
+}
+if(registrModal != null){
+    registrModal.addEventListener("click", function(){
+        let pass = prompt("Введите пароль администратора", );
+        if (pass == '123456789'){
+            alert("Регистрация прошла успешно")
+        }
+    })
+}
 $(document).ready(function() {
     $('.menu-burger__header').click(function() {
         $('.menu-burger__header').toggleClass('open-menu');
         $('.header_row').toggleClass('open-menu');
     });
 });
+
 let inpSearch = document.querySelector(".search_input");
 inpSearch.addEventListener("keyup", function(){
     const cards = document.querySelectorAll(".row__table");
@@ -44,7 +52,6 @@ inpSearch.addEventListener("keyup", function(){
         cards.forEach(function(item){
             let cardTitle = item.querySelector(".fio").textContent;
             if(!((cardTitle.toLowerCase()).includes(name.toLowerCase()))){
-                console.log(cardTitle + " - " + name);
                 item.classList.add("hidden");
             }else{
                 item.classList.remove("hidden");
